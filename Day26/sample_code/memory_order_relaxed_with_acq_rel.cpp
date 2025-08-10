8c3e51c2a853d98e1640ab227552e9ba090006b1
steven715 <yugioh821113@gmail.com>
2025-08-10 14:24:15 +0800

    原子操作文章完成

---
+++ Day26/sample_code/memory_order_relaxed_with_acq_rel.cpp
@@ -0,0 +1,36 @@
+#include <thread>
+#include <atomic>
+#include <assert.h>
+
+using namespace std;
+
+atomic<bool> x, y;
+atomic<int> z;
+
+void write_x_then_y()
+{
+    x.store(true, memory_order_relaxed);
+    y.store(true, memory_order_release);    
+}
+
+void read_y_then_x()
+{
+    while (!y.load(memory_order_acquire))
+        ;
+    if (x.load(memory_order_relaxed))
+        ++z;
+}
+
+int main(int argc, char const *argv[])
+{
+    x = false;
+    y = false;
+    z = 0;
+    thread a(write_x_then_y);
+    thread b(read_y_then_x);
+    a.join();
+    b.join();
+    assert(z.load() != 0);
+
+    return 0;
+}
---
+++ Day26/sample_code/memory_order_relaxed_with_fence.cpp
@@ -0,0 +1,38 @@
+#include <thread>
+#include <atomic>
+#include <assert.h>
+
+using namespace std;
+
+atomic<bool> x, y;
+atomic<int> z;
+
+void write_x_then_y()
+{
+    x.store(true, memory_order_relaxed);
+    atomic_thread_fence(memory_order_release);
+    y.store(true, memory_order_relaxed);    
+}
+
+void read_y_then_x()
+{
+    while (!y.load(memory_order_relaxed))
+    ;
+    atomic_thread_fence(memory_order_acquire);
+    if (x.load(memory_order_relaxed))
+        ++z;
+}
+
+int main(int argc, char const *argv[])
+{
+    x = false;
+    y = false;
+    z = 0;
+    thread a(write_x_then_y);
+    thread b(read_y_then_x);
+    a.join();
+    b.join();
+    assert(z.load() != 0);
+
+    return 0;
+}
